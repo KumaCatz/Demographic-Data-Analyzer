d9dfc9ffe36081d4d399e9cd44514c16da17c6f0
KumaCatz K <tod.kagan@gmail.com>
2024-11-30 19:14:02 +0000

    feat: race_count + avrg_age_men

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -4,12 +4,14 @@ import pandas as pd
 def calculate_demographic_data(print_data=True):
     # Read data from file
     df = pd.read_csv('adult.data.csv')
+    # print(df)
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
+    race_count = df['race'].value_counts()
+    race_count.name = None
 
     # What is the average age of men?
-    average_age_men = None
+    average_age_men = df[df['sex'] == 'Male']['age'].mean()
 
     # What is the percentage of people who have a Bachelor's degree?
     percentage_bachelors = None
@@ -68,5 +70,4 @@ def calculate_demographic_data(print_data=True):
         'top_IN_occupation': top_IN_occupation
     }
 
-example = calculate_demographic_data()
-print(example)
+calculate_demographic_data()
